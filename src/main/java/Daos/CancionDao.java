15ffcb8f97d91cf6f1cbd45fbee09ad4a174a8c7
joelsilvera <joel.silvera@pucp.edu.pe>
2022-06-13 20:06:06 -0500

    actualizacion

--- src/main/java/Daos/CancionDao.java
+++ src/main/java/Daos/CancionDao.java
@@ -1,10 +1,14 @@
 package Daos;
 
+import Beans.Cancion;
+
 import java.sql.*;
 import java.util.ArrayList;
 
 public class CancionDao {
-    public ArrayList<BandaDao> listarCanciones(){
+    public ArrayList<Cancion> listarCanciones(){
+        ArrayList<Cancion> listaCanciones = new ArrayList<>();
+
         String user = "root";
         String pass = "root";
         String url = "jdbc:mysql://localhost:3306/hr";
@@ -14,6 +18,26 @@ public class CancionDao {
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
+
+        try (Connection connection = DriverManager.getConnection(url, user, pass);
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT idcancion as 'ID', nombre_cancion as 'Cancion', nombre_banda as 'Banda' " +
+                     "FROM cancion c inner join banda b on (c.banda = b.idbanda) " +
+                     "ORDER by idcancion;");) {
+
+            while (rs.next()) {
+                Cancion cancion = new Cancion();
+                cancion.setIdCancion(rs.getString(1));
+                cancion.setNombre_cancion(rs.getString(2));
+                cancion.setNombre_banda(rs.getInt(3));
+                listaCanciones.add(cancion);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return listaCanciones;
+
 
     }
 }
